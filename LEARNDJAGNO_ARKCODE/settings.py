5e0e365c9dbfc4fd028f6af263a322af8ebee848
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-12-16 13:28:26 +0530

    update logout page

--- LEARNDJAGNO_ARKCODE/settings.py
+++ LEARNDJAGNO_ARKCODE/settings.py
@@ -143,18 +143,18 @@ USE_TZ = True
 
 
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER = 'priyanshukaushik6919@gmail.com'
-EMAIL_HOST_PASSWORD='tntd scfs ogbx smua'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True  
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'\
+# EMAIL_HOST='smtp.gmail.com'
+# EMAIL_HOST_USER = 'priyanshukaushik6919@gmail.com'
+# EMAIL_HOST_PASSWORD='tntd scfs ogbx smua'
+# EMAIL_PORT=587
+# EMAIL_USE_TLS=True  
+# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'\
 
 
 
 
 
-EMAIL_DEBUG = True
+# EMAIL_DEBUG = True
 
 
 
